f7e7eee4a4e8afed1554812328ea7dd17887353d
vy3191 <venkyafhs@gmail.com>
2019-10-19 22:38:51 -0400

    Added reactbootstrap alret

--- reducer-todo/src/components/Todo.js
+++ reducer-todo/src/components/Todo.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Alert from 'react-bootstrap/Alert'
 
 function Todo(props) {
   return (
-    <div>      
-      <p>{props.todo.item}</p>
+    <div> 
+      <Alert variant='warning' style={{textAlign:"left", paddingLeft:"60px"}}>{props.todo.item}</Alert>  
     </div>
   )
 }
